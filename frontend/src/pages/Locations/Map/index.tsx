b1452c1c72b38163e24b5fd436b96dc1f0f48cc6
Andrew Kuczynski <kuczynskiandrew@gmail.com>
2022-01-26 20:04:50 -0500

    clean up

--- frontend/src/pages/Locations/Map/index.tsx
+++ frontend/src/pages/Locations/Map/index.tsx
@@ -4,7 +4,6 @@ import L, { LatLngTuple, LocationEvent } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 import { Location } from '../../../types/location'
-import { LeafletMouseEvent } from 'leaflet';
 
 type MapProps = {
   locations: Location[]
@@ -18,10 +17,11 @@ var currentLocationIcon = L.divIcon({
   iconSize: [36, 36],
 });
 
-var PoiIcon = L.divIcon({
+var poiIcon = L.divIcon({
   className: 'loc',
   html: '📍',
   iconSize: [36, 36],
+  popupAnchor: [0, -18],
 });
 
 
@@ -31,17 +31,14 @@ export default function Map({locations}: MapProps) {
 
   return (
       <MapContainer center={position} zoom={14} style={{height: '100%'}} doubleClickZoom={false} whenCreated={(map) => {
-        
-        map.on('click', (e: LeafletMouseEvent) => {
-          console.log(e)
-          //setUserLocation([e.latlng.lat, e.latlng.lng])
-        })
-
         map.on('locationfound', (e: LocationEvent) => {
           console.log(e)
           setUserLocation([e.latlng.lat, e.latlng.lng])
         })
-        map.locate()
+        map.locate({
+          watch: true,
+          enableHighAccuracy: true
+        })
       }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -55,8 +52,8 @@ export default function Map({locations}: MapProps) {
       {userLocation && <Marker position={userLocation} icon={currentLocationIcon}/>}
       {
         locations.map(({id, coordinates, name}) => {
-          return (<Marker key={id} position={coordinates} icon={PoiIcon}>
-            <Popup>
+          return (<Marker key={id} position={coordinates} icon={poiIcon}>
+            <Popup closeButton={false}>
               {name}
             </Popup>
           </Marker>)
