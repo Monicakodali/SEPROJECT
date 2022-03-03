119e05acbfe27390b2eb007abe5218911bf2dca3
carolnavya <36771418+carolnavya@users.noreply.github.com>
2022-03-03 17:31:35 +0530

    Revert "Login page and landing page"

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,21 +1,13 @@
 import * as React from 'react';
-import * as ReactDOM from "react-dom"
-import "./index.css";
-import logo from "./gator.png";
-import { TopNav } from "./TopNav/TopNav";
-import { SearchSuggestions } from "./SearchSuggestions/SearchSuggestions";
+
+
 export default function LandingPage() {
   
   
 
   return (
-    <div className="index">
-      <h1 className='input'> HAPPY HAP'S YELP </h1>
-      <TopNav />
-      <img src={logo} className={logo} alt="logo" />
-       <input type="text" placeholder="Find..." className='input_box'/> 
-       <button className='input_submit' type='submit'> Go </button>
-       <SearchSuggestions />
+    <div>
+      landing page content
     </div>
   );
 };
