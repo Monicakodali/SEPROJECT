c8f53f9c4e35585140560ac396ab6b1c4ed3cee3
carolnavya <carolnavya1999@gmail.com>
2022-03-30 05:50:48 -0400

    Altered User table

--- api/models/user.go
+++ api/models/user.go
@@ -1,9 +1,11 @@
 package models
 
 type User struct {
-	Username string `json:"Username"`
-	Email    string `json:"Email"`
-	Name     string `json:"Name"`
-	Password string `json:"Password"`
-	Verified string `json:"Verified"`
+	Username  string `json:"Username"`
+	Email     string `json:"Email"`
+	Password  string `json:"Password"`
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+	Address   string `json:"Address"`
+	Verified  string `json:"Verified"`
 }
